bbc13ecbaf910a1857386552a0e3591ce608e369
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-08-24 18:52:10 -0300

    Bug Fix Challlenge and Save State

--- app/src/main/java/com/magalhaes/cauculator/MainActivity.kt
+++ app/src/main/java/com/magalhaes/cauculator/MainActivity.kt
@@ -8,11 +8,15 @@ import android.widget.EditText
 import android.widget.TextView
 import java.lang.NumberFormatException
 
+private const val STATE_PENDING_OPERATION = "PendingOperation"
+private const val STATE_OPERAND1 = "Operand1"
+private const val STATE_OPERAND1_STORED = "Operand1_Stored"
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var result: EditText
     private lateinit var newNumber: EditText
-    private val displyOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
+    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     //variables to hold the operands and type of calculation
     private var operand1: Double? = null
@@ -73,7 +77,7 @@ class MainActivity : AppCompatActivity() {
 
 
             pendingOperation = op
-            displyOperation.text = pendingOperation
+            displayOperation.text = pendingOperation
         }
 
         buttonEquals.setOnClickListener(opListener)
@@ -109,6 +113,28 @@ class MainActivity : AppCompatActivity() {
         result.setText(operand1.toString())
         newNumber.setText("")
 
-        displyOperation.text = operation
+        displayOperation.text = operation
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if (operand1 != null) {
+            outState.putDouble(STATE_OPERAND1, operand1!!)
+            outState.putBoolean(STATE_OPERAND1_STORED, true)
+        }
+        outState.putString(STATE_PENDING_OPERATION, pendingOperation)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        operand1 = if (savedInstanceState.getBoolean(STATE_OPERAND1_STORED, false)) {
+            savedInstanceState.getDouble(STATE_OPERAND1)
+
+        } else {
+            null
+        }
+
+        pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)!!
+        displayOperation.text = pendingOperation
     }
 }
