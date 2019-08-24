c2e6ce559c81ccedbaace9d5ab05c6bba1530072
Fabricio Magalhães <fabricio.lvm@gmail.com>
2019-08-24 19:27:23 -0300

    Widget References in Kotlin

--- app/src/main/java/com/magalhaes/cauculator/MainActivity.kt
+++ app/src/main/java/com/magalhaes/cauculator/MainActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import java.lang.NumberFormatException
+
+import kotlinx.android.synthetic.main.activity_main.*
 
 private const val STATE_PENDING_OPERATION = "PendingOperation"
 private const val STATE_OPERAND1 = "Operand1"
@@ -14,9 +14,6 @@ private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var result: EditText
-    private lateinit var newNumber: EditText
-    private val displayOperation by lazy(LazyThreadSafetyMode.NONE) { findViewById<TextView>(R.id.operation) }
 
     //variables to hold the operands and type of calculation
     private var operand1: Double? = null
@@ -26,28 +23,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        result = findViewById(R.id.result)
-        newNumber = findViewById(R.id.newNumber)
-
-        //Data input buttons
-        val button0: Button = findViewById(R.id.button0)
-        val button1: Button = findViewById(R.id.button1)
-        val button2: Button = findViewById(R.id.button2)
-        val button3: Button = findViewById(R.id.button3)
-        val button4: Button = findViewById(R.id.button4)
-        val button5: Button = findViewById(R.id.button5)
-        val button6: Button = findViewById(R.id.button6)
-        val button7: Button = findViewById(R.id.button7)
-        val button8: Button = findViewById(R.id.button8)
-        val button9: Button = findViewById(R.id.button9)
-        val buttonDot: Button = findViewById(R.id.buttonDot)
-
-        //Operation Buttons
-        val buttonEquals = findViewById<Button>(R.id.buttonEquals)
-        val buttonDivide = findViewById<Button>(R.id.buttonDivide)
-        val buttonMultiply = findViewById<Button>(R.id.buttonMutiply)
-        val buttonMinus = findViewById<Button>(R.id.buttonMinus)
-        val buttonPlus = findViewById<Button>(R.id.buttonPlus)
 
         val listener = View.OnClickListener { v ->
             val b = v as Button
@@ -77,25 +52,26 @@ class MainActivity : AppCompatActivity() {
 
 
             pendingOperation = op
-            displayOperation.text = pendingOperation
+            operation.text = pendingOperation
         }
 
         buttonEquals.setOnClickListener(opListener)
         buttonDivide.setOnClickListener(opListener)
-        buttonMultiply.setOnClickListener(opListener)
+        buttonMutiply.setOnClickListener(opListener)
         buttonMinus.setOnClickListener(opListener)
         buttonPlus.setOnClickListener(opListener)
 
 
     }
 
-    private fun performOperation(value: Double, operation: String) {
+    private fun performOperation(value: Double, op: String) {
 
         if (operand1 == null) {
             operand1 = value
         } else {
             if (pendingOperation == "=") {
-                pendingOperation = operation
+                pendingOperation = operation.text.toString()
+                print(operation.toString())
             }
             when (pendingOperation) {
                 "=" -> operand1 = value
@@ -113,7 +89,7 @@ class MainActivity : AppCompatActivity() {
         result.setText(operand1.toString())
         newNumber.setText("")
 
-        displayOperation.text = operation
+        operation.text = op
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -135,6 +111,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         pendingOperation = savedInstanceState.getString(STATE_PENDING_OPERATION)!!
-        displayOperation.text = pendingOperation
+        operation.text = pendingOperation
     }
 }
